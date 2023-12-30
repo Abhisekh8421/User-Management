0ae918270b3863a803909e0f959afcf2417abed0
Suru Abhisekh <abhisekhsuru@gmail.com>
2023-12-30 21:11:20 +0530

    user model added

--- models/user_model.js
+++ models/user_model.js
@@ -3,4 +3,42 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["Admin", "User"],
+      default: "User",
+    },
+    ProfileImage: {
+      type: String,
+    },
+    PhoneNumber: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
