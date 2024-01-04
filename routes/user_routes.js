0d088174a05eb7505920f24184bef41f8ce222c4
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-01-04 20:59:30 +0530

    generating new refresh token controllers

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,6 +3,7 @@ import { upload } from "../middlewares/multer_middleware.js";
 import {
   LoginUser,
   LogoutUser,
+  RefreshAccessToken,
   RegisterUser,
 } from "../controllers/user_controller.js";
 import isAuthenticated from "../middlewares/user_Auth.js";
@@ -14,5 +15,7 @@ router.route("/register").post(upload.single("ProfileImage"), RegisterUser);
 router.route("/login").post(LoginUser);
 
 router.route("/logout").get(isAuthenticated, LogoutUser);
+
+router.route("/refresh-token").post(RefreshAccessToken);
 
 export default router;
