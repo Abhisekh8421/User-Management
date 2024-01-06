89319ebf130e2522d7b76aada0e77514e43b168b
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-01-06 21:32:29 +0530

    debugging completed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "ds4z1p9c8",
-  api_key: "699777772934381",
-  api_secret: "Ey9rv2Re9rvpU7QRq_Q4235df7I",
+  cloud_name: process.env.CLOUDNAME, // replace  with your cloudinary  cloud name
+  api_key: process.env.API_KEY, // replace with your cloudinary API key
+  api_secret: process.env.API_SECRET, // replace with your cloudinary API Secret
 });
 
 export const uploadOnCloudinary = async (localpath) => {
@@ -16,7 +16,7 @@ export const uploadOnCloudinary = async (localpath) => {
       resource_type: "auto",
     });
     fs.unlinkSync(localpath);
-    // console.log("response", responce); for debugging purpose 
+    // console.log("response", responce); for debugging purpose
     return responce;
   } catch (error) {
     console.log("error is encountered ", error.message);
